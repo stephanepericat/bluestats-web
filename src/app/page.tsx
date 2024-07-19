5d2ef70a1f2c257ea1e9e9eae07c66b9a0343073
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-07-18 22:37:43 -0500

    getSession

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-// import useBluesky from '@/lib/useBluesky'
+import Link from 'next/link'
+import useBluesky from '@/lib/useBluesky'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
+  const { getSession } = useBluesky()
+  const session = getSession()
+
   return (
     <main>
-      <h1 className='text-2xl font-bold uppercase tracking-widest mb-3'>Blue Stats</h1>
-      <Button>Click Me!</Button>
+      {session ? (
+        <h1 className='text-2xl font-bold uppercase tracking-widest mb-3'>Blue Stats</h1>
+      ) : (
+        <Link href='/api/auth/signout'>Sign Out</Link>
+      )}
     </main>
   )
 }
--- src/lib/useBluesky.ts
+++ src/lib/useBluesky.ts
@@ -1,7 +1,12 @@
 import { agent } from './api'
 
 export default () => {
+  const getSession = () => {
+    return agent.session
+  }
+
   return {
     agent,
+    getSession,
   }
 } 
